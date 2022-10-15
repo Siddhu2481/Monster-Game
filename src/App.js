37fdf5de2c10dbb6f360c7fcb6aa0b6255e45fb9
siddharth sharma <siddharth.x24@gmail.com>
2022-10-15 11:59:49 +0530

    card

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import React,{ Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component{
   constructor()
   {
     super();
     this.state={
-      monsters:[
-        {
-          name: 'Siddharth'
-        },
-        {
-          name: 'Arjun'
-        },
-        {
-          name: 'Himanshu'
-        },
-      ]
+      monsters:[]
     };
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(users=>this.setState({monsters:users}));
   }
   render(){
      
       return (
         <div className="App">
+          <CardList />
           {
-            this.state.monsters.map(monster => )
-          }
+            this.state.monsters.map(monster => (
+            <h1 key={monster.id}> {monster.name}</h1>
+            ) )}
+          
         </div>
       );
     }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+export const CardList = (props) => {
+    console.log(props);
+    return (<div>Hello</div>);
+};
